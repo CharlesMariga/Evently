9e932877e150cd2b309ac0d7209d1edc973ffc0e
charlesmariga <charlesmariga37@gmail.com>
2024-02-11 18:41:53 +0300

    fix: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/events/:id",
-    "/api/webhooks/clerk(.*)",
-    "/api/uploadthing",
-  ],
+  publicRoutes: ["/", "/events/:id", "/api/webhooks(.*)", "/api/uploadthing"],
   ignoredRoutes: [
-    "/api/webhooks/clerk(.*)",
+    "/api/webhooks(.*)",
     "/api/webhooks/clerk",
     "/api/webhooks/stripe",
     "/api/uploadthing",
